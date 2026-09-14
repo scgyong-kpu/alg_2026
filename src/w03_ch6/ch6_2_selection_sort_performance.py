503d99aca044f8e70555b1fcf6dd5131450d5880
KKY@MBP16 <scgyong@tukorea.ac.kr>
2026-09-14 23:03:34 +0900

    선택정렬: 성능 측정 예제에서 시각화 제거

--- src/w03_ch6/ch6_2_selection_sort_performance.py
+++ src/w03_ch6/ch6_2_selection_sort_performance.py
@@ -1,11 +1,3 @@
-import pyvisalgo as va
-
-
-DATA_FILE = "data/elementary_sort.json"
-
-vis = va.visualizer("selection_sort")
-
-
 def selection_sort(array):
     # 정렬 함수는 전달받은 리스트를 직접 바꿉니다.
     # 선택 정렬은 아직 정렬되지 않은 구간에서 가장 작은 값을 찾아 앞쪽으로 옮깁니다.
@@ -16,29 +8,13 @@ def selection_sort(array):
     for left in range(n):
         # 처음에는 left 위치의 값을 최솟값 후보로 둡니다.
         min_at = left
-        vis.selection(min_at)
 
         # left 오른쪽의 아직 정렬되지 않은 값들을 끝까지 비교합니다.
         for right in range(left + 1, n):
-            vis.compare(min_at, right)
             if array[min_at] > array[right]:
                 min_at = right
-                vis.selection(min_at)
 
         # 남은 구간에서 찾은 최솟값을 left 위치로 옮깁니다.
-        vis.swap(left, min_at)
         array[left], array[min_at] = array[min_at], array[left]
-        vis.mark_sorted(left)
 
     return array
-
-
-while va.running():
-    data = va.next_data(__file__, data_file=DATA_FILE)
-    array = list(data.array)
-
-    vis.setup(data)
-    print("정렬 전:", array)
-    print("정렬 후:", selection_sort(array))
-    vis.finish()
-    vis.wait()
