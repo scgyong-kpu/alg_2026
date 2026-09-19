2f89978eaf695a7370f9139e95d33dc1ec6868b7
KKY@MBP16 <scgyong@tukorea.ac.kr>
2026-09-19 10:43:40 +0900

    힙 정렬: root와 왼쪽 자식의 값을 비교한다
    root index 0의 왼쪽 자식 index를 2 * root + 1로 계산한다. 왼쪽 자식이 배열 안에 있을 때 부모와 비교해 max heap 조건을 처음으로 확인한다.

--- src/w03_ch6/ch6_5_heap_sort.py
+++ src/w03_ch6/ch6_5_heap_sort.py
@@ -14,6 +14,13 @@ def heap_sort(array):
     # 배열 index를 완전 이진 트리의 부모와 자식 관계로 보여 줍니다.
     vis.build_tree()
 
+    # index 0을 root로 잡으면 왼쪽 자식의 index는 2 * root + 1입니다.
+    root = 0
+    left = root * 2 + 1
+    vis.set_root(root)
+    if left < count:
+        vis.compare(root, left)
+
     return array
 
 
